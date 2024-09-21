4db55ac4826a1186e40e39e326a3b6fb933c289a
edgard <edg@alpine.my.domain>
2024-09-21 01:29:20 +0100

    Ajout de postgres

--- django_docker/settings.py
+++ django_docker/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django_docker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'mypassword',
+        'HOST':'mydb',
+        'PORT': '5432',
     }
 }
 
